8cd4153c8f991844ae7958aeb6e489d82b9d3d3b
Adrian Romański <adrianromanski@icloud.com>
2023-11-22 09:11:55 +0100

    feat: backend, proxy settings

--- apps/doctor-flight/src/app/app.config.ts
+++ apps/doctor-flight/src/app/app.config.ts
@@ -1,7 +1,14 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { HttpClientModule, provideHttpClient } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(appRoutes)]
+  providers: [
+    provideRouter(appRoutes),
+    importProvidersFrom(HttpClientModule),
+    provideAnimations(),
+    provideHttpClient()
+  ]
 };
